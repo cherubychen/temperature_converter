9d4ef2af9251da8aee12f7634c5247f0ab3cc42f
cherubychen <cheruby.chen@gmail.com>
2016-04-23 19:05:38 +0800

    this is my initial commit

---
+++ src/main/java/temperature_converter/temperature_converter/App.java
@@ -0,0 +1,13 @@
+package temperature_converter.temperature_converter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
