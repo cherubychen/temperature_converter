5d67a6a5eaeb7e5f539421a2f24bf0113a9f674f
cherubychen <cheruby.chen@gmail.com>
2016-04-23 19:34:15 +0800

    cucumber file

---
+++ target/cucumber/report.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri('com\packtpub\mastering\selenium\Conversion.feature');
+formatter.feature({
+  "line": 1,
+  "name": "Convert between different unit of temperatures",
+  "description": "\r\nFormula: C/5 \u003d (F - 32) / 9\r\nC \u003d 5(F - 32)/9 \u003d 5(98.6 - 32)/9 \u003d 37",
+  "id": "convert-between-different-unit-of-temperatures",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 6,
+  "name": "Convert to Celsius from Fahrenheit",
+  "description": "",
+  "id": "convert-between-different-unit-of-temperatures;convert-to-celsius-from-fahrenheit",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 8,
+  "name": "I want to convert 98.6 degree Fahrenheit to Celsius",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 9,
+  "name": "I input the value of Fahrenheit as 98.6 in text",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "It should be converted to Celsius as 37 degree",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "98",
+      "offset": 18
+    },
+    {
+      "val": "6",
+      "offset": 21
+    }
+  ],
+  "location": "ConversionStepDefs.I_want_to_convert_degree_Fahrenheit_to_Celsius(int,int)"
+});
+formatter.result({
+  "duration": 97125823,
+  "error_message": "cucumber.runtime.PendingException: TODO: implement me\r\n\tat com.packtpub.mastering.selenium.steps.ConversionStepDefs.I_want_to_convert_degree_Fahrenheit_to_Celsius(ConversionStepDefs.java:13)\r\n\tat ✽.Given I want to convert 98.6 degree Fahrenheit to Celsius(com\\packtpub\\mastering\\selenium\\Conversion.feature:8)\r\n",
+  "status": "pending"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "98",
+      "offset": 35
+    },
+    {
+      "val": "6",
+      "offset": 38
+    }
+  ],
+  "location": "ConversionStepDefs.I_input_the_value_of_Fahrenheit_as_in_text(int,int)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "37",
+      "offset": 37
+    }
+  ],
+  "location": "ConversionStepDefs.It_should_be_converted_to_Celsius_as_degree(int)"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
